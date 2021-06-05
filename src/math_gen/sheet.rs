3f76af801509be39ee5048b4b59b48c6a8ba463d
NoohAlavi <noohalavi@gmail.com>
2021-06-05 18:13:09 -0400

    Updated comments, header is now different on q/a sheets

--- src/math_gen/sheet.rs
+++ src/math_gen/sheet.rs
@@ -17,8 +17,16 @@ impl Sheet {
         }
     }
 
-    pub fn get_header(&self) -> String {
-        format!("-NOOH'S MATH GENERATOR-\nSHEET ID {}\nNAME:\nDATE:\n\n", self.id)
+    pub fn get_header(&self, is_question_sheet: bool) -> String {
+        let mut header = format!("-NOOH'S MATH GENERATOR-\nSHEET ID {}\n", self.id);
+        
+        if is_question_sheet {
+            header.push_str("NAME:\nDATE:\n\n");
+        } else {
+            header.push('\n');
+        }
+        
+        header
     }
     
     pub fn save_to_file(&self) -> std::io::Result<()>{
@@ -31,19 +39,19 @@ impl Sheet {
 
         //Format arrays to strings
         let mut questions_str = String::new();
-        questions_str.push_str(&self.get_header());
+        questions_str.push_str(&self.get_header(true));
         questions_str.push_str(&self.questions.join("\n"));
         
         let mut answers_str = String::new();
-        answers_str.push_str(&self.get_header());
+        answers_str.push_str(&self.get_header(false));
         answers_str.push_str(&self.answers.join("\n"));
 
-        // TODO generate file with questions
+        // generate file with questions
 
         let mut questions_file = File::create(file_path_questions)?;
         questions_file.write_all(&questions_str.as_bytes())?;
 
-        // TODO generate file with answers
+        // generate file with answers
 
         let mut answers_file = File::create(file_path_answers)?;
         answers_file.write_all(&answers_str.as_bytes())?;
